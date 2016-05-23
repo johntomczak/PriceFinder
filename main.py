c40b542bd1dc24bea4587b09ec440c607b168f8f
John J Tomczak <john.jr.tomczak@me.com>
2016-05-23 13:16:12 -0400

    Monday Morning Update
    Small improvements to the timing system and the date-checker, which is
    not yet complete.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import prices
 import datetime
+import time
 
 def run( today ) :
 	prices.prices( today )
 
 def main() :
 	i = 1
-	while( i<2 ) :
+	while( i == 1 ) :
+		weekday = datetime.datetime.now().weekday()
+		nw = datetime.datetime.now().time()
 		today = datetime.date.today()
-		i+=1
-
-	run( today )
+		# print weekday, nw, today
+		i += 1
+		if( weekday < 9 ) :
+			if( nw > datetime.time( 16, 25, 0, 0, tzinfo=None ) ) :     # 4.25
+				if( nw < datetime.time( 17, 0, 0, 0, tzinfo=None ) ) :	# 5.00
+					#this will need to be wrapped in a try / catch
+					print "runnning now"
+					run( today )
+		
+		print "waiting"
+		# time.sleep( 1800 ) #wait 30 minutes and then sight the window again
 
 ######
 main()
--- prices.py
+++ prices.py
@@ -11,25 +11,22 @@ def prices( today):
 	newP = "&column_index=4"
 
 	response = urllib2.urlopen( main + code + baseP + newP ) # open connection
-	readr = csv.reader( response )                   		 # recognize CSV
-	inx = list( readr )    						     		 # make "array" - all simplified in following
-	inx = inx[1][1]								     		 # make into single value
-	inx = round( float( inx ), 2 )							 # str to float, cut to x.xx
-	# print inx
+	readr = csv.reader( response ) 							 # recognize CSV
+	lst = list(readr)										 # make "array" - all simplified in following
+	inxDay = lst[1][0]										 # store the day
+	inx = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+
 
 	### DJIA - curl "https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_DJI.csv?rows=1&api_key=4rPBKrUryJJ5wJjwfH9Y&column_index=4" -X GET
 	code = "YAHOO/INDEX_DJI"
 	newP = "&column_index=4"
 
 	response = urllib2.urlopen( main + code + baseP + newP ) # open connection
-	response = urllib2.urlopen( main + code + baseP + newP ) # open connection
+	readr = csv.reader( response ) 							 # recognize CSV
+	lst = list(readr)										 # make "array" - all simplified in following
+	djiDay = lst[1][0]										 # store the day
+	dji = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
 
-	readr = csv.reader( response ) 
-	lst = list(readr)
-	day = lst[1][0]
-	dji = round( float( lst[1][1] ), 2 )	 		 # simplified assignment (see SP500)
-	print day
-	print dji
 
 	### WTI
 	code = "CHRIS/CME_CL1"
@@ -37,8 +34,10 @@ def prices( today):
 
 	response = urllib2.urlopen( main + code + baseP + newP ) # open connection
 	readr = csv.reader( response )                   		 # recognize CSV
-	wti = round( float( list( readr )[1][1] ), 2 )   		 # simplified assignment (see SP500)
-	# print wti
+	lst = list(readr)										 # make "array" - all simplified in following
+	wtiDay = lst[1][0]										 # store the day
+	wti = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+
 
 	### Brent
 	code = "CHRIS/ICE_B1"
@@ -46,8 +45,10 @@ def prices( today):
 
 	response = urllib2.urlopen( main + code + baseP + newP ) # open connection
 	readr = csv.reader( response )                   		 # recognize CSV
-	brent = round( float( list( readr )[1][1] ), 2 ) 		 # simplified assignment (see SP500)
-	# print brent
+	lst = list(readr)										 # make "array" - all simplified in following
+	brentDay = lst[1][0]										 # store the day
+	brent = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+
 
 	### Gold
 	code = "CHRIS/CME_GC1"
@@ -55,8 +56,10 @@ def prices( today):
 
 	response = urllib2.urlopen( main + code + baseP + newP ) # open connection
 	readr = csv.reader( response )                   		 # recognize CSV
-	gold = round( float( list( readr )[1][1] ), 2 )	 		 # simplified assignment (see SP500)
-	# print gold
+	lst = list(readr)										 # make "array" - all simplified in following
+	goldDay = lst[1][0]										 # store the day
+	gold = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+
 
 	### VIX
 	code = "CBOE/VIX"
@@ -64,19 +67,21 @@ def prices( today):
 
 	response = urllib2.urlopen( main + code + baseP + newP ) # open connection
 	readr = csv.reader( response )                   		 # recognize CSV
-	vix = round( float( list( readr )[1][1] ), 2 )	 		 # simplified assignment (see SP500)
-	# print vix
+	lst = list(readr)										 # make "array" - all simplified in following
+	vixxDay = lst[1][0]										 # store the day
+	vix = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+
 
 	### loop the Treasuries
 
 	code = "USTREASURY/YIELD"
-	govt = [0,0,0,0,0,0,0,0,0,0,0]
+	yields = [0,0,0,0,0,0,0,0,0,0,0]
 
 	for i in range(0, 11):
 		newP = "&column_index=" + str(i+1)
 		response = urllib2.urlopen( main + code + baseP + newP ) # open connection
 		readr = csv.reader( response )                   		 # recognize CSV
-		govt[i] = round( float( list( readr )[1][1] ), 2 )	 	 # simplified assignment (see SP500)
+		yields[i] = round( float( list( readr )[1][1] ), 2 )	 	 # simplified assignment (see SP500)
 
 	### construct USDXY - need USD to EUR, JPY, GBP, CAD, SEK, CHF - like the rest
 
