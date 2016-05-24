635b8d73fc8b054d7b5e4eaafe0df9f704e81f4f
John J Tomczak <john.jr.tomczak@me.com>
2016-05-24 00:41:36 -0400

    Overnight testing 1
    Trying to watch as the new numbers get posted. Also some small
    improvements, mainly in syntax.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def run( today ) :
 	prices.prices( today )
 
 def main() :
-	i = 1
+	
 	while( 1 == 1 ) :
 		weekday = datetime.datetime.now().weekday()
 		nw = datetime.datetime.now().time()
@@ -14,14 +14,14 @@ def main() :
 		# print weekday, nw, today
 		i += 1
 		if( weekday < 5 ) :
-			if( nw > datetime.time( 16, 25, 0, 0, tzinfo=None ) ) :     # 4.25
-				if( nw < datetime.time( 17, 0, 0, 0, tzinfo=None ) ) :	# 5.00
+			if( nw > datetime.time( 16, 05, 30, 0, tzinfo=None ) ) :     # 4.15
+				if( nw < datetime.time( 22, 15, 0, 0, tzinfo=None ) ) :	# 6.15
 					#this will need to be wrapped in a try / catch
-					print "runnning now"
+					print "running now", nw
 					run( today )
 		
-		print "waiting", nw
-		time.sleep( 1200 ) #wait 30 minutes and then sight the window again
+		print "waiting 10 minutes from - ", nw
+		time.sleep( 600 ) #wait 30 minutes and then sight the window again
 
 ######
 main()
--- prices.py
+++ prices.py
@@ -15,6 +15,7 @@ def prices( today):
 	lst = list(readr)										 # make "array" - all simplified in following
 	inxDay = lst[1][0]										 # store the day
 	inx = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+	# print inx
 
 
 	### DJIA - curl "https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_DJI.csv?rows=1&api_key=4rPBKrUryJJ5wJjwfH9Y&column_index=4" -X GET
@@ -26,6 +27,7 @@ def prices( today):
 	lst = list(readr)										 # make "array" - all simplified in following
 	djiDay = lst[1][0]										 # store the day
 	dji = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+	# print dji
 
 
 	### WTI
@@ -37,17 +39,19 @@ def prices( today):
 	lst = list(readr)										 # make "array" - all simplified in following
 	wtiDay = lst[1][0]										 # store the day
 	wti = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+	# print wti
 
 
 	### Brent
 	code = "CHRIS/ICE_B1"
-	newP = "&column_index=6"
+	newP = "&column_index=4"
 
 	response = urllib2.urlopen( main + code + baseP + newP ) # open connection
 	readr = csv.reader( response )                   		 # recognize CSV
 	lst = list(readr)										 # make "array" - all simplified in following
 	brentDay = lst[1][0]										 # store the day
 	brent = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+	# print brent
 
 
 	### Gold
@@ -59,6 +63,7 @@ def prices( today):
 	lst = list(readr)										 # make "array" - all simplified in following
 	goldDay = lst[1][0]										 # store the day
 	gold = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+	# print gold
 
 
 	### VIX
@@ -70,6 +75,7 @@ def prices( today):
 	lst = list(readr)										 # make "array" - all simplified in following
 	vixxDay = lst[1][0]										 # store the day
 	vix = round( float( lst[1][1] ), 2 )	 		 		 # simplified assignment (see SP500)
+	# print vix
 
 
 	### loop the Treasuries
@@ -137,6 +143,7 @@ def prices( today):
 
 	dxy = 50.14348112 * pow(eur, -0.576) * pow(jpy, 0.136) * pow(gbp, -0.119) * pow(cad, 0.091) * pow(sek, 0.042) * pow(chf, 0.036)
 	dxy = round( dxy, 4 )
+	# print dxy
 
 
 	print "S&P 500 			=", inx
@@ -146,6 +153,6 @@ def prices( today):
 	print "gold    			=", gold
 	print "VIX     			=", vix
 	print "dxy				=", dxy
-	print "Treasury yields  		=", govt
+	print "Treasury yields  		=", yields
 
 #####
